762608d0708ca457dcb18cc143be8d0617ac6ae3
adexfit <y.adeboye@gmail.com>
2025-07-24 18:08:33 +0100

    added background image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Menu from "@/components/Menu";
 import { useTaskStore } from "@/store/useTaskStore";
 import NewTask from "@/components/NewTask";
 import AllNotes from "@/components/AllNotes";
-import UpdateTask from "@/components/UpdateTask";
+// import UpdateTask from "@/components/UpdateTask";
 
 export default function Home() {
   const { theme } = useTheme();
@@ -14,8 +14,9 @@ export default function Home() {
 
   console.log(theme);
   return (
-    <div className="min-h-screen bg-blue-500 px-6 py-8 dark:bg-gray-800">
-      <div className="mx-auto h-auto w-full rounded-xl bg-white md:w-2/3 lg:w-1/2 dark:bg-gray-700">
+    <div className="relative min-h-screen bg-[url(/bg.jpg)] bg-cover bg-center px-6 py-8">
+      <div className="absolute inset-0 bg-blue-500/50 dark:bg-gray-800/80"></div>
+      <div className="relative z-10 mx-auto h-auto w-full rounded-xl bg-white md:w-2/3 lg:w-1/2 dark:bg-gray-700">
         <Header />
         {!newTaskPage && <Menu />}
         {!newTaskPage && <hr className="text-gray-100 dark:text-gray-600" />}
@@ -26,4 +27,6 @@ export default function Home() {
   );
 }
 
-// [#E3E3E3]
+// [#E3E3E3]   min-h-screen
+// "bg-[url(/bg.jpg)]"
+// bg-blue-900/80
