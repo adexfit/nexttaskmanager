16a171b0001b7bffec892bea13bb0fca66b49fd9
adexfit <y.adeboye@gmail.com>
2025-07-25 07:03:38 +0100

    edited ui colors for main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,13 @@ import ThemeSwitch from "./ThemeSwitch";
 const Header = () => {
   return (
     <>
-      <div className="flex items-center justify-between rounded-t-lg bg-gray-100 px-6 py-2 dark:bg-gray-700">
+      <div className="flex items-center justify-between rounded-t-lg bg-gray-100 px-6 py-2 dark:bg-gray-800">
         <h1 className="text-sm font-bold text-gray-600 dark:text-white">
           Task Manager
         </h1>
         <ThemeSwitch />
       </div>
+      <hr className="text-gray-100 dark:text-gray-600" />
     </>
   );
 };
