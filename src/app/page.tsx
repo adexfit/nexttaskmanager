592aa69911c1251357437da29f90dadf1a40553d
adexfit <y.adeboye@gmail.com>
2025-07-25 07:08:53 +0100

    reduced y padding on home page for mobile users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const newTaskPage = useTaskStore((state) => state.newTaskPage);
 
   return (
-    <div className="relative min-h-screen bg-[url(/bg.jpg)] bg-cover bg-center px-2 py-8">
+    <div className="relative min-h-screen bg-[url(/bg.jpg)] bg-cover bg-center px-2 py-2 md:py-8">
       <div className="absolute inset-0 bg-blue-500/50 dark:bg-gray-800/80"></div>
       <div className="relative z-10 mx-auto h-auto w-full rounded-xl bg-white md:w-2/3 lg:w-1/2 dark:bg-gray-800">
         <Header />
