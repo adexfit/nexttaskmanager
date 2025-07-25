77ee703912859f0681e45cb00ce305b879866054
adexfit <y.adeboye@gmail.com>
2025-07-25 05:01:51 +0100

    mobile ui optimization done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
 
   console.log(theme);
   return (
-    <div className="relative min-h-screen bg-[url(/bg.jpg)] bg-cover bg-center px-6 py-8">
+    <div className="relative min-h-screen bg-[url(/bg.jpg)] bg-cover bg-center px-2 py-8">
       <div className="absolute inset-0 bg-blue-500/50 dark:bg-gray-800/80"></div>
-      <div className="relative z-10 mx-auto h-auto w-full rounded-xl bg-white md:w-2/3 lg:w-1/2 dark:bg-gray-700">
+      <div className="relative z-10 mx-auto h-auto w-full rounded-xl bg-white md:w-2/3 lg:w-1/2 dark:bg-gray-800">
         <Header />
         {!newTaskPage && <Menu />}
         {!newTaskPage && <hr className="text-gray-100 dark:text-gray-600" />}
@@ -26,7 +26,3 @@ export default function Home() {
     </div>
   );
 }
-
-// [#E3E3E3]   min-h-screen
-// "bg-[url(/bg.jpg)]"
-// bg-blue-900/80
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import ThemeSwitch from "./ThemeSwitch";
 const Header = () => {
   return (
     <>
-      <div className="flex items-center justify-between rounded-t-lg bg-gray-100 px-6 py-2 dark:bg-gray-600">
+      <div className="flex items-center justify-between rounded-t-lg bg-gray-100 px-6 py-2 dark:bg-gray-700">
         <h1 className="text-sm font-bold text-gray-600 dark:text-white">
           Task Manager
         </h1>
